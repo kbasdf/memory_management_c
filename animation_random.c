d9be7c1b319f899586f74c0fac1810fd8fa0370d
kbasdf <kashishbhasin99@yahoo.com>
2025-06-25 00:46:29 +0530

    adding animation file

---
+++ animation_random.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("\n\n\n\n\n\n\n\n\n\n");
+    int count1=1;
+    int zool=0;
+
+
+    here:;
+    int new_line=0;
+    printf("count :::: %d",count1);
+    printf("\n");
+    new_line++;
+    printf("\n");
+    new_line++;
+    printf("\n");  // <----- pointer is here
+    new_line++;
+    int count=400000000;
+
+    if((count1%3)==0){
+        printf("\033[A");
+        new_line--;
+        printf(" <3");
+        printf("\n");
+        new_line++;
+        zool=1;
+    }
+    else if (zool==1){
+            printf("\033[A");
+            new_line--;
+            printf("\r");
+            printf("\033[A");
+            new_line--;
+            printf("\r");
+        printf(" <3");
+        printf("\n");
+        new_line++;
+        printf("\n");
+        new_line++;
+        zool=0;
+
+    }
+
+    printf(" 0");
+    printf("\n");
+    new_line++;
+    if (count1%2==0){
+        printf("/|\\_");
+        printf("\n");
+        new_line++;
+    }
+    else{
+        printf("/|\\ ");
+        printf("\n");
+        new_line++;
+    }
+    printf(" |");
+    printf("\n");
+    new_line++;
+    printf("/ \\");
+
+    // moving the cursor back to original position
+    // -----
+    while(count!=0){
+        count--;
+    }
+    int ff=new_line;
+    while(ff>0){
+    printf("\033[A");
+    ff--;
+    }
+    int i=0;
+    while(i !=new_line){
+        printf("     \n");
+        i++;
+    }
+
+    //printf("\033[A");
+
+    while(new_line !=0){
+         printf("\033[A");
+         new_line--;
+    }
+    printf("\r");
+        //printf("\033[A");
+    //printf("\033[A");
+
+    count1++;
+    goto here;
+    return 0;
+}
+
